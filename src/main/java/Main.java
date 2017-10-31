481d24a86831ca17c695efe38a1be4acfceb7f4a
Jan-Eike <j-e_brocker@t-online.de>
2017-10-31 21:26:23 +0100

    Added BinaryTreePrinter and BinryTree Class, to save the function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
 public class Main{
 	public static void main(String[] args) {
+		Node<String> root = new Node<String>("*");
+		Node<String> left = new Node<String>("exp");
+		Node<String> leftRight = new Node<String>("x");
+		Node<String> right = new Node<String>("pol");
+		Node<String> rightRight = new Node<String>("3");
+		Node<String> rightLeft = new Node<String>("+");
+		Node<String> leftRightLeft = new Node<String>("2x");
+		Node<String> leftRightRight = new Node<String>("3");
+		
+		root.left = left;
+		root.left.right = leftRight;
+		root.right = right;
+		root.right.right = rightRight;
+		root.right.left = rightLeft;
+		root.left.right.left = leftRightLeft;
+		root.left.right.right = leftRightRight;
+		
+		BTreePrinter.printNode(root);
 		System.out.println("Hello World!");
 	}
 }
