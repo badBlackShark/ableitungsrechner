e9c03108baf399c869af24bc39a17b4f1790a304
kaigeissler <kai.geissler@hhu.de>
2017-10-31 22:09:35 +0100

    Added Javafx Interface

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
-public class Main{
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("Oberflaeche.fxml"));
+		primaryStage.setTitle("Ableitungsrechner");
+		primaryStage.setScene(new Scene(root, 1024, 720));
+		primaryStage.show();
 	}
 }
---
+++ src/main/java/OberflaecheController.java
@@ -0,0 +1,24 @@
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class OberflaecheController implements Initializable{
+	
+	@FXML private Button EingabeButton;
+	@FXML private TextField EingabeFeld;
+	@FXML private TextArea Ausgabefenster;
+
+	@Override
+	public void initialize(java.net.URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
+
+
