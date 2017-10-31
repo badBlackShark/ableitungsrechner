f2bb86d805a13a9c13c54a48236024a22db95ac4
Jan-Eike <j-e_brocker@t-online.de>
2017-10-31 21:39:21 +0100

    fixed error in tree construction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,16 +6,16 @@ public class Main{
 		Node<String> right = new Node<String>("pol");
 		Node<String> rightRight = new Node<String>("3");
 		Node<String> rightLeft = new Node<String>("+");
-		Node<String> leftRightLeft = new Node<String>("2x");
-		Node<String> leftRightRight = new Node<String>("3");
+		Node<String> rightLeftLeft = new Node<String>("2x");
+		Node<String> rightLeftRight = new Node<String>("3");
 		
 		root.left = left;
 		root.left.right = leftRight;
 		root.right = right;
 		root.right.right = rightRight;
 		root.right.left = rightLeft;
-		root.left.right.left = leftRightLeft;
-		root.left.right.right = leftRightRight;
+		root.right.left.left = rightLeftLeft;
+		root.right.left.right = rightLeftRight;
 		
 		BTreePrinter.printNode(root);
 		System.out.println("Hello World!");
