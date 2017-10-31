bfb41aab8be294c7d69232b1c1f0dd14a09165e4
Jan-Eike <j-e_brocker@t-online.de>
2017-10-31 21:43:27 +0100

    bigger example for the tree

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,15 @@
 public class Main{
 	public static void main(String[] args) {
 		Node<String> root = new Node<String>("*");
-		Node<String> left = new Node<String>("exp");
-		Node<String> leftRight = new Node<String>("x");
-		Node<String> right = new Node<String>("pol");
+		Node<String> left = new Node<String>("*");
+		Node<String> leftRight = new Node<String>("e");
+		Node<String> right = new Node<String>("p");
 		Node<String> rightRight = new Node<String>("3");
 		Node<String> rightLeft = new Node<String>("+");
 		Node<String> rightLeftLeft = new Node<String>("2x");
 		Node<String> rightLeftRight = new Node<String>("3");
+		Node<String> leftLeft = new Node<String>("3");
+		Node<String> leftRightRight = new Node<String>("x");
 		
 		root.left = left;
 		root.left.right = leftRight;
@@ -16,6 +18,8 @@ public class Main{
 		root.right.left = rightLeft;
 		root.right.left.left = rightLeftLeft;
 		root.right.left.right = rightLeftRight;
+		root.left.left = leftLeft;
+		root.left.right.right = leftRightRight;
 		
 		BTreePrinter.printNode(root);
 		System.out.println("Hello World!");
